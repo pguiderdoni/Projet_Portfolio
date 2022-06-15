de901018034f07b7f4e756e04de381f1dfe82697
pguiderdoni <101812669+pguiderdoni@users.noreply.github.com>
2022-06-15 11:09:52 +0200

    15/06 A 1

---
+++ main.js
@@ -0,0 +1,12 @@
+let cv = document.getElementById("cv");
+cv.style.cursor = "zoom-in";
+cv.addEventListener("click", function () {
+  console.log(cv);
+  if (cv.classList.contains("h-100")) {
+    cv.classList.replace("h-100", "h-auto");
+    cv.style.cursor = "zoom-out";
+  } else if (cv.classList.contains("h-auto")) {
+    cv.classList.replace("h-auto", "h-100");
+    cv.style.cursor = "zoom-in";
+  }
+});
